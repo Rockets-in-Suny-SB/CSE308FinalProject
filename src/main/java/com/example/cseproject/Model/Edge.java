95cf9048d836ac7cce4f1c429b18dcf4d755a0b4
Canjie Sun <cansun@cs.stonybrook.edu>
2019-11-25 21:29:05 -0500

    modify edge entity

--- src/main/java/com/example/cseproject/Model/Edge.java
+++ src/main/java/com/example/cseproject/Model/Edge.java
@@ -6,11 +6,10 @@ import javax.persistence.*;
 public class Edge {
 
     private Integer id;
-    private Integer adjacentPrecinct1_id;
-    private Integer adjacentPrecinct2_id;
+    private Integer selfPrecinctId;
+    private Integer adjacentPrecinctId;
     private Boolean sameCounty;
     private Float demographicSimilarity;
-    private Integer precinct_id;
 
     @Id
     @Column(name = "edge_id")
@@ -22,20 +21,20 @@ public class Edge {
         this.id = id;
     }
 
-    public Integer getAdjacentPrecinct1_id() {
-        return adjacentPrecinct1_id;
+    public Integer getSelfPrecinctId() {
+        return selfPrecinctId;
     }
 
-    public void setAdjacentPrecinct1_id(Integer adjacentPrecinct1_id) {
-        this.adjacentPrecinct1_id = adjacentPrecinct1_id;
+    public void setSelfPrecinctId(Integer selfPrecinctId) {
+        this.selfPrecinctId = selfPrecinctId;
     }
 
-    public Integer getAdjacentPrecinct2_id() {
-        return adjacentPrecinct2_id;
+    public Integer getAdjacentPrecinctId() {
+        return adjacentPrecinctId;
     }
 
-    public void setAdjacentPrecinct2_id(Integer adjacentPrecinct2_id) {
-        this.adjacentPrecinct2_id = adjacentPrecinct2_id;
+    public void setAdjacentPrecinctId(Integer adjacentPrecinctId) {
+        this.adjacentPrecinctId = adjacentPrecinctId;
     }
 
     public Boolean getSameCounty() {
@@ -52,13 +51,5 @@ public class Edge {
 
     public void setDemographicSimilarity(Float demographicSimilarity) {
         this.demographicSimilarity = demographicSimilarity;
-    }
-
-    public Integer getPrecinct_id() {
-        return precinct_id;
-    }
-
-    public void setPrecinct_id(Integer precinct_id) {
-        this.precinct_id = precinct_id;
     }
 }
--- src/main/java/com/example/cseproject/Service/EdgeService.java
+++ src/main/java/com/example/cseproject/Service/EdgeService.java
@@ -15,19 +15,19 @@ public class EdgeService {
 
     public Optional<Edge> getEdge(Integer id){return edgeRepository.findById(id);}
 
-    public String addEdge(Integer id, Integer adjacentPrecinct1_id, Integer adjacentPrecinct2_id,
-                          Boolean sameCounty, Float demographicSimilarity, Integer precinct_id){
-        Edge edge = new Edge();
-        edge.setId(id);
-        edge.setDemographicSimilarity(demographicSimilarity);
-        edge.setPrecinct_id(precinct_id);
-        edge.setAdjacentPrecinct1_id(adjacentPrecinct1_id);
-        edge.setAdjacentPrecinct2_id(adjacentPrecinct2_id);
-        edge.setSameCounty(sameCounty);
-//
-        edgeRepository.save(edge);
-        return "Saved";
-    }
+//    public String addEdge(Integer id, Integer adjacentPrecinct1_id, Integer adjacentPrecinct2_id,
+//                          Boolean sameCounty, Float demographicSimilarity, Integer precinct_id){
+//        Edge edge = new Edge();
+//        edge.setId(id);
+//        edge.setDemographicSimilarity(demographicSimilarity);
+//        edge.setPrecinct_id(precinct_id);
+//        edge.setAdjacentPrecinct1_id(adjacentPrecinct1_id);
+//        edge.setAdjacentPrecinct2_id(adjacentPrecinct2_id);
+//        edge.setSameCounty(sameCounty);
+////
+//        edgeRepository.save(edge);
+//        return "Saved";
+//    }
 
     public String deleteEdge(Edge edge){
         edgeRepository.delete(edge);
