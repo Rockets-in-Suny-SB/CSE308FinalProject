801579b1cbe3598657ce0746641351c0cb3644e6
ericlin94 <eric50818244@gmail.com>
2019-12-16 03:08:56 -0500

    Cluster data

--- src/main/java/com/example/cseproject/DataClasses/ClusterData.java
+++ src/main/java/com/example/cseproject/DataClasses/ClusterData.java
@@ -33,11 +33,31 @@ public class ClusterData {
         this.minorityGroupPopulation = minorityGroupPopulation;
     }
 
+    public int getGopVotes() {
+        return gopVotes;
+    }
+
+    public void setGopVotes(int gopVotes) {
+        this.gopVotes = gopVotes;
+    }
+
+    public int getDemVotes() {
+        return demVotes;
+    }
+
+    public void setDemVotes(int demVotes) {
+        this.demVotes = demVotes;
+    }
+
+    private int gopVotes;
+    private int demVotes;
     private int population;
     private Map<DemographicGroup, Integer> minorityGroupPopulation;
     public ClusterData(Cluster c){
         this.id=c.getId();
         this.population=c.getPopulation();
         this.minorityGroupPopulation=c.getMinorityGroupPopulation();
+        this.gopVotes=c.getGopVotes();
+        this.demVotes=c.getDemVotes();
     }
 }
